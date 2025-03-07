8f6210fbf7d4bd4868fd69da8fa6a32e869aab77
macyabbey <macy.abbey@gmail.com>
2025-03-06 18:08:51 -0800

    wire up pheonix persistence for working with datasets, prompts and evals

--- examples/init-examples-environment.sh
+++ examples/init-examples-environment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 docker container stop examples-pgadmin-1
 docker container stop examples-postgres-db-1
+docker container stop examples-pheonix-1
 docker container rm examples-pgadmin-1
 docker container rm examples-postgres-db-1
+docker container rm examples-pheonix-1
 docker volume rm examples_db_data
+docker-compose pull
 docker-compose up -d --build
